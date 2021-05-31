5e476c69568c63cdd6d3f3ee4a85a1c0ab0efbfc
Tejas Girhe <77229114+TejasGirhe@users.noreply.github.com>
2021-05-31 20:16:29 +0530

    assg2

--- estimate.py
+++ estimate.py
@@ -25,7 +25,7 @@ class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
             pi = wallis(i)
-            self.assertFalse(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
+            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
             
     def test_high_iters(self):
         for i in range(500, 600):
@@ -37,9 +37,7 @@ class TestMC(unittest.TestCase):
     def test_randomness(self):
         pi0 = monte_carlo(15000)
         pi1 = monte_carlo(15000)
-        
         self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
-
         self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
 
     def test_accuracy(self):
